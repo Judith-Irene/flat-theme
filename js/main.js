1a91c7086e802630146fc49b7a78ebe5b832fc4b
judith1997 <judithirene17@gmail.com>
2016-01-08 05:33:57 -0500

    trabajo parap portafokio

---
+++ js/main.js
@@ -0,0 +1,40 @@
+jQuery(function ($) {
+
+    $('.centered').each(function (e) {
+        $(this).css('margin-top', ($('#main-slider').height() - $(this).height()) / 2);
+    });
+
+    $(window).resize(function () {
+        $('.centered').each(function (e) {
+            $(this).css('margin-top', ($('#main-slider').height() - $(this).height()) / 2);
+        });
+    });
+
+
+    //contact form
+    var form = $('.contact-form');
+    form.submit(function () {
+        $this = $(this);
+        $.post($(this).attr('action'), function (data) {
+            $this.prev().text(data.message).fadeIn().delay(3000).fadeOut();
+        }, 'json');
+        return false;
+    });
+
+    //goto top
+    $('gototop').click(function (event) {
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: $("body").offset().top
+        }, 500);
+    });
+});
+
+    $(document).ready(initPage);
+    	function initPage(){
+    		$('#arrow-up').click(irArriba);
+    	}
+    	function irArriba(evt){
+    		TweenMax.to('body, html', 5 , {  scrollTo:{y:0},ease:Quart.easeOut});
+            evt.preventDefault();
+        }
